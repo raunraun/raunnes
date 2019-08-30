6937c086691fc6c336450c64f66808ebb6966cb7
raun <raun@raun>
2019-08-29 21:39:45 -0500

    Logger stubs

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,4 +1,4 @@
-
+#include "Logger.h"
 
 namespace raunnes {
 
--- src/Logger.h
+++ src/Logger.h
@@ -1,4 +1,6 @@
+#include <cstdint>
 
+#include <string>
 
 namespace raunnes {
 
@@ -7,6 +9,22 @@ class Logger {
 public:
 	Logger();
 	~Logger();
+
+    void LogCurrentState(
+        uint16_t Address,
+        uint8_t Byte0,
+        uint8_t Byte1,
+        uint8_t Byte2,
+        const std::string& OpCodeDsiassemble,
+        uint8_t A,
+        uint8_t X,
+        uint8_t Y,
+        uint8_t P,
+        uint8_t SP,
+        uint16_t PPU,
+        uint64_t cycle);
+
+private:
 
 };
 
