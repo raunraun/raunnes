504d4318eca7ba4d3271561b34f3e23ac893d7e7
raunraun <raun.krisch@gmail.com>
2021-02-01 20:05:02 -0600

    Updates to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
 #include <iostream>
 
+#include "6502Core.h"
+#include "MemoryMap.h"
+
 int main(int argc, char** argv) {
     
     std::fstream romFile("../tests/nestest/nestest.nes", std::ios::in | std::ios::binary);
