4a8c5d4fb4b5b60850281c747d9364da0494262f
raunraun <raun.krisch@gmail.com>
2021-02-01 21:00:20 -0600

    New memory implementation

--- src/MemoryMap.cpp
+++ src/MemoryMap.cpp
@@ -4,88 +4,33 @@
 #include <cassert>
 
 namespace raunnes {
-
-MemoryMapEntry::MemoryMapEntry(MemoryMap const& map, uint32_t start, uint32_t end) :
-    m_Map(map),
-    m_Start(start),
-    m_End(end) {
-}
-
-bool MemoryMapEntry::contains(uint32_t address) const {
-    return address >= m_Start && address <= m_End;
-}
-
-
-
-
-MemoryMapEntryAlwaysFault::MemoryMapEntryAlwaysFault(MemoryMap const& map) :
-   MemoryMapEntry(map, 0, 0) {
-}
-
-uint8_t MemoryMapEntryAlwaysFault::operator[](uint32_t address) {
-    assert(0);
-    return 0;
-}
-
-
-
-MemoryMapEntryRAM::MemoryMapEntryRAM(MemoryMap const& map, uint32_t start, uint32_t end) :
-    MemoryMapEntry(map, start, end) {
-
-    uint32_t size = end - start;
-    m_Bytes.resize(size);
-}
-
-uint8_t MemoryMapEntryRAM::operator[](uint32_t address) {
-    uint8_t a = m_Start - address;
-    return m_Bytes[a];
-}
-
-
-
-
-MemoryMapEntryROM::MemoryMapEntryROM(MemoryMap const& map, uint32_t start, uint32_t end) :
-    MemoryMapEntry(map, start, end) {
-
-    uint32_t size = end - start;
-    m_Bytes.resize(size);
-}
-
-uint8_t MemoryMapEntryROM::operator[](uint32_t address) {
-    uint8_t a = m_Start - address;
-    return m_Bytes[a];
-}
-
-
-
-
-MemoryMap::MemoryMap() :
-    m_AlwaysFaultEntry(*this) {
-
+MemoryMap::MemoryMap() {
+	m_Bytes.resize(0x10000);
 }
 
 MemoryMap::~MemoryMap() {
 
 }
 
-void MemoryMap::map(uint32_t start, uint32_t end, MMIOCallback cb) {
-   // m_Entries.push_back(new MemoryMapEntry(*this, start, end));
+uint16_t MemoryMap::Read16(uint16_t address) {
+	if (address+1 < m_Bytes.size()) {
+		uint16_t val = ((uint16_t)m_Bytes[address]) | (uint16_t(m_Bytes[address+1]) >> 8);
+		return val;
+	}
+	return 0;
 }
-
-void MemoryMap::ram(uint32_t start, uint32_t end) {
-    m_Entries.push_back(new MemoryMapEntryRAM(*this, start, end));
+	
+uint16_t MemoryMap::Read(uint16_t address) {
+	if (address < m_Bytes.size()) {
+		return m_Bytes[address];
+	}
+	return 0;
 }
-
-void MemoryMap::rom(uint32_t start, uint32_t end) {
-    m_Entries.push_back(new MemoryMapEntryROM(*this, start, end));
-}
-
-MemoryMapEntry& MemoryMap::operator[](uint32_t address) {
-    auto map = std::find_if(m_Entries.begin(),
-        m_Entries.end(),
-        [&address](const MemoryMapEntry* mme) { return true; });
-    
-    return (map != m_Entries.end()) ? **map : m_AlwaysFaultEntry;
+	
+void MemoryMap::Write(uint16_t address, uint8_t value) {
+	if (address < m_Bytes.size()) {
+		return m_Bytes[address] = value;
+	}
 }
 
 }
--- src/MemoryMap.h
+++ src/MemoryMap.h
@@ -6,73 +6,23 @@
 
 namespace raunnes {
 
-typedef std::function<void(uint32_t, uint32_t)> MMIOCallback;
-
-class MemoryMapEntry {
-
-    friend class MemoryMap;
-public:
-    MemoryMapEntry(MemoryMap const& map, uint32_t start, uint32_t end);
-    ~MemoryMapEntry() = default;
-
-    virtual uint8_t operator[](uint32_t address) = 0;
-    bool contains(uint32_t address) const;
-
-protected:
-    MemoryMap const&  m_Map;
-    uint32_t          m_Start;
-    uint32_t          m_End;
-};
-
-
-
-
-class MemoryMapEntryAlwaysFault : public MemoryMapEntry {
-public:
-    MemoryMapEntryAlwaysFault(MemoryMap const& map);
-
-    uint8_t operator[](uint32_t address) override;
-};
-
-
-
-
-class MemoryMapEntryRAM : public MemoryMapEntry {
-public:
-    MemoryMapEntryRAM(MemoryMap const& map, uint32_t start, uint32_t end);
-    uint8_t operator[](uint32_t address) override;
-
-private:
-    std::vector<uint8_t> m_Bytes;
-};
-
-
-
-class MemoryMapEntryROM : public MemoryMapEntry {
-public:
-    MemoryMapEntryROM(MemoryMap const& map, uint32_t start, uint32_t end);
-    uint8_t operator[](uint32_t address) override;
-
-private:
-    std::vector<uint8_t> m_Bytes;
-};
-
-
-
-
 class MemoryMap {
 public:
-    MemoryMap();
-    ~MemoryMap();
+	MemoryMap();
+	~MemoryMap();
 
-    void map(uint32_t start, uint32_t end, MMIOCallback);
-    void ram(uint32_t start, uint32_t end);
-    void rom(uint32_t start, uint32_t end);
-    MemoryMapEntry& operator[](uint32_t address);
+	uint16_t Read16(uint16_t address);
+	uint16_t Read(uint16_t address);
+	void Write(uint16_t address, uint8_t value);
+
+
+public:
+	MemoryMap(const MemoryMap&) = delete;
+	MemoryMap& operator=(const MemoryMap&) = delete;
 
 private:
-    std::vector<MemoryMapEntry*> m_Entries;
-    MemoryMapEntryAlwaysFault m_AlwaysFaultEntry;
+	std::vector<uint8_t> m_Bytes;
 };
+
 
 }
