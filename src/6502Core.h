51cfe0bd249f1b7fec31dc098349ca55b1227548
raunraun <raun.krisch@gmail.com>
2021-02-02 21:37:18 -0600

    Refactoring

--- src/6502Core.h
+++ src/6502Core.h
@@ -5,36 +5,37 @@
 #include "MemoryMap.h"
 
 namespace raunnes {
-class CPUCore6502State {
-public:
-	uint8_t A;
-	uint8_t X;
-	uint8_t Y;
-
-	union {
-		uint8_t S;
-		struct {
-			uint8_t C : 1;
-			uint8_t Z : 1;
-			uint8_t I : 1;
-			uint8_t D : 1;
-			uint8_t B : 1;
-			uint8_t U : 1;
-			uint8_t V : 1;
-			uint8_t N : 1;
-		};
-	};
-
-	uint16_t PC;
-	uint16_t SP;
-
-	void SetFlags(uint8_t Flag) {
-		S = Flag;
-	}
-};
 
 class CPUCore6502 {
 public:
+	class CPUCore6502State {
+	public:
+		uint8_t A;
+		uint8_t X;
+		uint8_t Y;
+
+		union {
+			uint8_t S;
+			struct {
+				uint8_t C : 1;
+				uint8_t Z : 1;
+				uint8_t I : 1;
+				uint8_t D : 1;
+				uint8_t B : 1;
+				uint8_t U : 1;
+				uint8_t V : 1;
+				uint8_t N : 1;
+			};
+		};
+
+		uint16_t PC;
+		uint16_t SP;
+
+		void SetFlags(uint8_t Flag) {
+			S = Flag;
+		}
+	};
+
 	struct DynamicExecutionInfo {
 		uint16_t PC;
 		uint16_t Address;
