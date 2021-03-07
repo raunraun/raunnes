3f92507897a800c27703a6e129ee8d3c31ceab95
raunraun <raun.krisch@gmail.com>
2021-03-07 15:01:18 -0600

    Implementing disasm for Accumulator addressing mode

--- src/main.cpp
+++ src/main.cpp
@@ -48,6 +48,10 @@ void log(const raunnes::CPUCore6502::InstructionDetails& info,
         s << std::uppercase << std::setw(2) << std::setfill('0') << std::hex << (uint32_t)map.Read(details.Address());
         s << std::setw(28 - 8) << std::setfill(' ');
         break;
+    case raunnes::CPUCore6502::AddressingModeAccumulator:
+        s << "A";
+        s << std::setw(28 - 1) << std::setfill(' ');
+        break;
     case raunnes::CPUCore6502::AddressingModeImmediate:
         s << "#$" << std::uppercase << std::setw(2) << std::setfill('0') << std::hex << (uint32_t)details.Immediate();
         s << std::setw(28 - 4) << std::setfill(' ');
